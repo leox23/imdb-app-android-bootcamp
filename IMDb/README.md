77ef491cf22ee11e3e5251ab8c96d8d586486bc2
leox23 <leox-23@hotmail.com>
2022-05-24 15:47:34 -0500

    Estructura de vista busqueda
    algunas inconsistencias con vistas android normales, el projecto fue creado en compose

--- IMDb/README.md
+++ IMDb/README.md
@@ -1,14 +1,14 @@
 # IMDb app
 
-🚧Vista Register en construccion🚧
+🚧Vista Busqueda en construccion🚧
 
 ### Pendientes:
 
-- [ ] Trabajar el modo de busqueda tipo activity con recycleView.
-- [ ] Corregir el alto del status bar.
+- [ ] Aplicar la navegacion entre vistas.
+- [ ] Corregir el alto del status bar, no parece ser un action var o algo.
 - [ ] Validacion de campos en login y register.
 - [ ] Colocar los textos de vista Login en res/values/strings.xml.
-- [ ] Trabajar las dimensiones desde res/dimen.xml.
+- [ ] Trabajar las dimensiones desde res/values/dimens.xml.
 
 ### Bugs
 
@@ -16,14 +16,8 @@
 
 ### Listos recientemente
 
-- [x] Pasar los argumentos de logos a ~~painterResource~~ Image(painter = ?) para tener solo un composable de logos, no logro concordar los tipos.
-- [x] Completar pantalla register
-- [x] Llenar el manifest (lo necesario hasta ahora).
-- [x] Modularizar @Composables a respectivos archivos.
-- [x] Quitar el color de la barra de estado en login y splash.
-- [x] Splash logo
-
-
+- [x] Trabajar la vista de modo de busqueda tipo AdroidView con recycleView (estructura).
+- [x] Modificar entorno para que pueda recibir activities y otros detalles (muchas inconsistencias porque el projecto fue creado en compose)
 
 
 
--- IMDb/app/src/main/java/com/example/imdb/MainActivity.kt
+++ IMDb/app/src/main/java/com/example/imdb/MainActivity.kt
@@ -8,26 +8,50 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.imdb.MovieProvider.Companion.movieList
+import com.example.imdb.adapter.MovieAdapter
 import com.example.imdb.ui.components.Greeting
 import com.example.imdb.ui.login.StandartLogin
 import com.example.imdb.ui.start_app.Register
 import com.example.imdb.ui.theme.IMDbTheme
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.recyclerMovies
+import kotlinx.android.synthetic.main.list_item_movie.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        setContentView(R.layout.activity_main)
+        //welcomeMessage.text = "Hello Kotlin desde el id directamten!"
+        val recyclerView = recyclerMovies
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MovieAdapter(MovieProvider.movieList)
+/*
         setContent {
             IMDbTheme {
                 Surface(
+
                     modifier = Modifier.fillMaxSize()
                 ) {
                     //Greeting()
-                    StandartLoginPreview()
+                    //StandartLoginPreview()
+                    //setContentView(R.layout.activity_main)
                 }
             }
         }
+
+
+ */
+
     }
 }
+
+
 
 /*
 //@Preview(showBackground = true)
@@ -40,7 +64,7 @@ fun GreetingPreview() {
 */
 
 
-
+/*
 @Preview(showBackground = true)
 @Composable
 fun StandartLoginPreview() {
@@ -51,4 +75,6 @@ fun StandartLoginPreview() {
     }
 }
 
+
+ */
 
